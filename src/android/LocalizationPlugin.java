a14a86f5df35673ad7e23215005c6a708aad97a7
sevinlim <sevin.lim@gmail.com>
2016-08-26 15:52:07 +0800

    Update plugin

--- src/android/LocalizationPlugin.java
+++ src/android/LocalizationPlugin.java
@@ -14,20 +14,12 @@
 
 package com.sldev.cordova.locale;
 
-import android.Manifest;
 import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothGattCharacteristic;
-import android.bluetooth.BluetoothManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.content.IntentFilter;
-import android.os.Handler;
 
-import android.provider.Settings;
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaArgs;
 import org.apache.cordova.CordovaPlugin;
@@ -42,82 +34,123 @@ import java.util.*;
 
 public class LocalizationPlugin extends CordovaPlugin {
 
-    // actions
-    private static final String ACTION_GET = "get";
-    private static final String ACTION_GET_ALL = "getAll";
+  // actions
+  private static final String ACTION_GET = "get";
+  private static final String ACTION_GET_ALL = "getAll";
 
-    private static final String TAG = "LocalizationPlugin";
+  private static final String TAG = "LocalizationPlugin";
 
-    private JSONObject stringMap = null;
+  private JSONObject stringMap = new JSONObject();
+  private BroadcastReceiver localeChangeReceiver = null;
 
-    private JSONObject getStringMap() throws Exception {
-        if (stringMap == null) {
-            Activity activity = cordova.getActivity();
-            stringMap = new JSONObject();
-            // int identifier = activity.getResources().getIdentifier(resourceId, "string", activity.getPackageName());
-            // activity.getResources().getString(identifier)
-            // for (Field field : R.string.class.getDeclaredFields()) {
-            //     if (Modifier.isStatic(field.getModifiers()) && !Modifier.isPrivate(field.getModifiers()) && field.getType().equals(int.class)) {
-            //         try {
-            //             int stringId = field.getInt(null);
-            //             stringMap.put(field.getName(), mContext.getResources().getString(stringId));
-            //         } catch (JSONException e) {
-            //             throw e.getMessage();
-            //         } catch (IllegalAccessException e) {
-            //             throw e.getMessage();
-            //         }
-            //     }
-            // }
-        }
-        return stringMap;
+//  ACTION_LOCALE_CHANGED
+
+  private void registerLocaleChange() {
+    if (localeChangeReceiver != null) {
+      return;
     }
-
-    public String getAll() {
-        return stringMap.toString();
-    }
-
-    public String get(String resourceId) {
-        if (!stringMap.has(resourceId)) {
-            return null;
+    Activity activity = cordova.getActivity();
+    IntentFilter filter = new IntentFilter(Intent.ACTION_LOCALE_CHANGED);
+    localeChangeReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        // update stored strings
+        Iterator<String> jsonKeys = stringMap.keys();
+        while (jsonKeys.hasNext()) {
+          String key = jsonKeys.next();
+          try {
+            stringMap.put(key, getFromResource(key));
+          } catch (JSONException e) {
+            LOG.e(TAG, "Can't update string with key: "+key);
+          }
         }
-        return stringMap.get(resourceId);
+      }
+    };
+      activity.registerReceiver(localeChangeReceiver, filter);
+  }
+
+  private String getFromResource(String resourceId) {
+    Activity activity = cordova.getActivity();
+    int identifier = activity.getResources().getIdentifier(resourceId, "string", activity.getPackageName());
+    if (identifier == 0) {
+      return null;
     }
+    String resString = activity.getResources().getString(identifier);
+    return resString;
+  }
 
-    // public String getLocale() {
-    //     return Locale.getDefault().getDisplayLanguage();
-    // }
+//  private JSONObject getStringMap() throws Exception {
+//    if (stringMap == null) {
+//      stringMap = new JSONObject();
+//       for (Field field : R.string.class.getDeclaredFields()) {
+//           if (Modifier.isStatic(field.getModifiers()) && !Modifier.isPrivate(field.getModifiers()) && field.getType().equals(int.class)) {
+//               try {
+//                   int stringId = field.getInt(null);
+//                   stringMap.put(field.getName(), mContext.getResources().getString(stringId));
+//               } catch (JSONException e) {
+//                   throw e.getMessage();
+//               } catch (IllegalAccessException e) {
+//                   throw e.getMessage();
+//               }
+//           }
+//       }
+//    }
+//    return stringMap;
+//  }
 
-    @Override
-    public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
-        if (stringMap == null) {
-            try {
-                getStringMap();
-            } catch (Exception e) {
-                callbackContext.error(e.getMessage());
-            }
-        }
+  public String getAll() {
+    return stringMap.toString();
+  }
 
-        if (action.equals(ACTION_GET)) {
-            String resourceId = args.getString(0);
-            String resString = get(resourceId);
-            if (resString != null) {
-                callbackContext.success(resString);
-            } else {
-                callbackContext.error("Resource not found: "+resourceId);
-                return false;
-            }
-        } else if (action.equals(ACTION_GET_ALL)) {
-            String resString = getAll();
-            if (resString == null) {
-                callbackContext.success(resString);
-            } else {
-                callbackContext.error("Resource file not found");
-                return false;
-            }
-        } else {
-            callbackContext.error("Invalid action");
-            return false;
-        }
-        return true;
+  public String get(String resourceId) throws JSONException {
+    String resString;
+    if (!stringMap.has(resourceId)) {
+      resString = getFromResource(resourceId);
+      if (resString != null) {
+        stringMap.put(resourceId, resString);
+      }
+    } else {
+      resString = stringMap.getString(resourceId);
     }
+    return resString;
+  }
+
+  // public String getLocale() {
+  //     return Locale.getDefault().getDisplayLanguage();
+  // }
+
+  @Override
+  public boolean execute(String action, CordovaArgs args, CallbackContext callbackContext) throws JSONException {
+    registerLocaleChange();
+//    if (stringMap == null) {
+//      try {
+////        getStringMap();
+//      } catch (Exception e) {
+//        callbackContext.error(e.getMessage());
+//      }
+//    }
+
+    if (action.equals(ACTION_GET)) {
+      String resourceId = args.getString(0);
+      String resString = get(resourceId);
+      if (resString != null) {
+        callbackContext.success(resString);
+      } else {
+        callbackContext.error("Resource not found: " + resourceId);
+        return false;
+      }
+    } else if (action.equals(ACTION_GET_ALL)) {
+      String resString = getAll();
+      if (resString == null) {
+        callbackContext.success(resString);
+      } else {
+        callbackContext.error("Resource file not found");
+        return false;
+      }
+    } else {
+      callbackContext.error("Invalid action");
+      return false;
+    }
+    return true;
+  }
 }
